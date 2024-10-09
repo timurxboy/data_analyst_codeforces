46852d4f0a3162e4d4438576be1dd630610de451
Timurxboy <timurxboy@gmail.com>
2024-10-10 00:51:14 +0300

    thread down to 3

--- service/parse/member_parse_from_problem.py
+++ service/parse/member_parse_from_problem.py
@@ -122,7 +122,7 @@ class MemberParse:
             with conn.cursor() as cur:
                 problems = self.get_problem(cur=cur)
                 
-                with ThreadPoolExecutor(max_workers=5) as executor:
+                with ThreadPoolExecutor(max_workers=3) as executor:
                     futures = []
                     
                     for i in range(11, len(problems) + 1):
